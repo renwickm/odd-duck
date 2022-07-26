1cdc47b84703f045a8cc9425861caa47d17e9c26
Matthew T. Renwick <mrenny65@gmail.com>
2022-07-25 19:08:03 -0600

    completed JS for lab 11

--- js/app.js
+++ js/app.js
@@ -23,7 +23,7 @@ let resultsList = document.getElementById('results-list');
 
 function Products(name, photoExtension = 'jpg') {
   this.name = name;
-  this.photo = `img/${name}.${photoExtension}`;
+  this.photo = `assets/${name}.${photoExtension}`;
   this.views = 0;
   this.votes = 0;
 
@@ -58,10 +58,68 @@ new Products('wine-glass');
 
 // ************* Helper Functions ***************
 
+function randomIndexGenerator(){
+  return Math.floor(Math.random() * allProducts.length);
+}
 
+function renderImgs(){
+  let imgOneIndex = randomIndexGenerator();
+  let imgTwoIndex = randomIndexGenerator();
+  let imgThreeIndex = randomIndexGenerator();
+
+  while(imgOneIndex === imgTwoIndex || imgOneIndex === imgThreeIndex || imgTwoIndex === imgThreeIndex){
+    imgTwoIndex = randomIndexGenerator();
+    imgThreeIndex = randomIndexGenerator();
+  }
+
+  imgOne.src = allProducts[imgOneIndex].photo;
+  imgOne.alt = allProducts[imgOneIndex].name;
+  allProducts[imgOneIndex].views++;
+
+  imgTwo.src = allProducts[imgTwoIndex].photo;
+  imgTwo.alt = allProducts[imgTwoIndex].name;
+  allProducts[imgTwoIndex].views++;
+
+  imgThree.src = allProducts[imgThreeIndex].photo;
+  imgThree.alt = allProducts[imgThreeIndex].name;
+  allProducts[imgThreeIndex].views++;
+}
+
+renderImgs();
 
 // ************* Event Handlers *****************
 
+function handleClick(event){ 
 
+  let imgClicked = event.target.alt;
+  
+  for(let i = 0; i < allProducts.length; i++){
+    if(imgClicked === allProducts[i].name){
+      allProducts[i].votes++;
+    }
+  }
+  totalVotes--;
+
+  renderImgs();
+
+  if(totalVotes === 0){
+    imgContainer.removeEventListener('click', handleClick);
+  }
+
+}
+
+function handleShowResults(){
+  if(totalVotes === 0){
+    for(let i = 0; i < allProducts.length; i++){
+      let liElem = document.createElement('li');
+      liElem.textContent = `${allProducts[i].name} had ${allProducts[i].votes} votes, and was seen ${allProducts[i].views} times.`;
+      resultsList.appendChild(liElem);
+    }
+    resultBtn.removeEventListener('click', handleShowResults);
+  }
+}
 
 // ************* Event Listeners ****************
+
+imgContainer.addEventListener('click', handleClick);
+resultBtn.addEventListener('click', handleShowResults);
